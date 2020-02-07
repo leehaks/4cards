d94deaebcd6e61e8ba8fd7af2834a5d7d7a897cc
leejunghak <jhlee@bitnine.net>
2020-02-07 22:43:33 +0900

    first commit 4card layout

---
+++ js/script.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+
+    var currentMain; 
+    var mainAnchor = document.querySelectorAll('.section-component');
+    
+    function mainAnchorHandler() { 
+    
+        mainAnchor.forEach( allAnchor => { 
+            allAnchor.classList.add('off');
+        })
+    
+        this.classList.replace('off','on');
+        this.querySelector('.content-component').classList.remove('hidden');
+        currentMain = this;            
+    }
+    
+    mainAnchor.forEach( anchor => {
+        anchor.addEventListener('click', mainAnchorHandler)
+    })
+}
