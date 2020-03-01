ac7359783f1c77225f4f1f77326ed214b9476e50
bugman001 <jhlee@bitnine.net>
2020-03-02 01:36:24 +0900

    scrollTo 적용 및 navbar 일부요소 적용

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ window.onload = function(){
     
         this.classList.replace('off','on');
         this.querySelector('.content-component').classList.remove('hidden');
-        this.querySelector('.title-component .con').classList.remove('hidden');
+        this.querySelector('.title-component').classList.add('hidden');
         currentMain = this;            
     }
     
@@ -19,43 +19,102 @@ window.onload = function(){
         anchor.addEventListener('click', mainAnchorHandler)
     })
 
-    var imgAnchor = document.querySelectorAll('.slider-img'); 
+    // tab-component
 
-    function imgAnchorFrame() { 
-        imgAnchor.forEach( currentFrame => {
-            frame = currentFrame.querySelector('.main-frame')
-            
-            frameWidth = frame.getAttribute('width');
-            frameHeight = frame.getAttribute('height');
-
-            frame.style.width = frameWidth + "px";  
-            frame.style.height = frameHeight + "px";              
-        })
-    }
-
-    imgAnchorFrame(); 
-
-    var currentTabItem; 
-    var tabItem = document.querySelectorAll('.tab-component'); 
+    var currentTabLabel, currentTabPanel; 
+    var tabComponent = document.querySelectorAll('.tab-component'); 
 
     function activate(elem){
         elem.classList.add('active'); 
-        currentTabItem = elem; 
+        currentTabLabel = elem; 
     }
 
+    function inactivate(elem){ 
+        elem.classList.remove('active'); 
+    }
+
+    function activatePanel(elem){
+        elem.classList.add('active'); 
+        currentTabPanel = elem; 
+    }
+    
     function tabHandler(e) { 
-        if (currentTabItem) { 
-            currentTabItem.classList.remove('active'); 
-        }
-        e.target.classList.add('active'); 
-        activate(e.target); 
+        const targetElem = e.target; 
+        const targetContain = targetElem.classList.contains('tab-label')
+
+        if (targetContain) { 
+            inactivate(currentTabLabel); 
+            inactivate(currentTabPanel); 
+
+            activate(targetElem);     
+            
+            let tabOffSetLeft = this.getBoundingClientRect().left.toFixed(2)
+
+            this.querySelector('.bar').style.left = 
+            targetElem.getBoundingClientRect().left.toFixed(2) - tabOffSetLeft +"px"; 
+            
+            console.log(tabOffSetLeft,targetElem.getBoundingClientRect().left.toFixed(2)); 
+            
+
+            let targetHref, targetPanel; 
+            targetHref = targetElem.getAttribute('href')
+            targetPanel = this.querySelector(targetHref); 
+            
+            activatePanel(targetPanel); 
+        }       
     }
 
-    tabItem.forEach(items => { 
-        var tabFirst = items.querySelector('a')
-        activate(tabFirst); 
+    tabComponent.forEach(items => { 
+        let tabLabelFirst = items.querySelector('.tab-label-group .tab-label');
+        let tabPanelFirst = items.querySelector('.tab-panel');
+        activate(tabLabelFirst); 
+        activatePanel(tabPanelFirst);
+
+        let labelBar = document.createElement('div'); 
+        labelBar.classList.add('bar');
+        items.querySelector('.tab-label-group').appendChild(labelBar); 
+
         items.addEventListener('click', tabHandler); 
     })
 
-    var scrollAction = document.querySelectorAll('.section-component')
+    // scroll 
+
+    let sectionElem = document.querySelectorAll('.section-component .section-box'); 
+    let navbarLinks = document.querySelectorAll('.navbar a')
+
+    navbarLinks.forEach(elem => elem.addEventListener('click', navbarLinkClick));
+
+    function navbarLinkClick(e) { 
+        event.preventDefault();
+        let targetId = e.currentTarget.getAttribute('href'); 
+
+        sectionElem.forEach( elem => { 
+            elem.scrollTo({
+                top: document.querySelector(targetId).offsetTop, 
+                behavior: "smooth"
+            }) ; 
+        }) 
+    }
+
+    let currentHeight = 0; 
+
+    function scrollHandler(e) { 
+        e.preventDefault();
+        
+        let scrollHeight = this.getBoundingClientRect().height; 
+
+        if(e.deltaY > 0) {
+            this.scrollTo({
+                top: currentHeight += scrollHeight, 
+                behavior: "smooth"
+            });  
+        }else{
+            this.scrollTo({
+                top: currentHeight -= scrollHeight, 
+                behavior: "smooth"
+            }); 
+        }
+    }
+
+    sectionElem.forEach( elem => elem.addEventListener('mousewheel', scrollHandler));
 }
